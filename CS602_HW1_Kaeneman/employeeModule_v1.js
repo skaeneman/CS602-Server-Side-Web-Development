dd69639ee1e5106b970693d9638e7ef22ce6046f
skaeneman <skaeneman@yahoo.cm>
2019-01-24 20:56:00 -0500

    mod1-hw1 started HW1

--- CS602_HW1_Kaeneman/employeeModule_v1.js
+++ CS602_HW1_Kaeneman/employeeModule_v1.js
@@ -1,1 +1,20 @@
-test
+// array of javascript objects
+var data = [
+    { id:1, firstName:'John', lastName:'Smith' },
+    { id:2, firstName:'Jane', lastName:'Smith' },
+    { id:3, firstName:'John', lastName:'Doe' },
+];
+
+module.exports = {
+    lookupById: () => {
+        return data.id;
+        //TODO: add undefined 
+    },
+    addEmployee: (firstName, lastName) => {
+        data.firstName = firstName;
+        data.lastName = lastName;
+        //calculate id
+    }    
+};
+
+
