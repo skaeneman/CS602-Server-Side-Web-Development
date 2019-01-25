6cf49abb47e3391da5ca3c60a7d7c4c2023be2d3
skaeneman <skaeneman@yahoo.cm>
2019-01-24 22:00:10 -0500

    module1-hw1 got lookupById function to work

--- CS602_HW1_Kaeneman/employeeModule_v1.js
+++ CS602_HW1_Kaeneman/employeeModule_v1.js
@@ -1,14 +1,23 @@
-// array of javascript objects
+
+// array of user objects
 var data = [
     { id:1, firstName:'John', lastName:'Smith' },
     { id:2, firstName:'Jane', lastName:'Smith' },
-    { id:3, firstName:'John', lastName:'Doe' },
+    { id:3, firstName:'John', lastName:'Doe' }
 ];
 
 module.exports = {
-    lookupById: () => {
-        return data.id;
-        //TODO: add undefined 
+    /*****************************************************************************
+    * Finds a user by searching for their id in an array.
+    * Argument: takes an integer id.
+    * Returns: first name, last name, and id of a user in the array, or undefined.
+    ******************************************************************************/
+    lookupById: (user_id) => {
+        for (var i in data) {
+            if (data[i].id == user_id) {
+                return data[i]; 
+            }
+        } 
     },
     addEmployee: (firstName, lastName) => {
         data.firstName = firstName;
---
+++ CS602_HW1_Kaeneman/hw1a.js
@@ -0,0 +1,3 @@
+var foo = require('./employeeModule_v1');
+
+console.log(foo.lookupById(2));
