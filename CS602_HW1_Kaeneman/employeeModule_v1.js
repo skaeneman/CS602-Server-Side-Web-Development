487c0f314b711f71d6350811df6cc7fba29357fa
skaeneman <skaeneman@yahoo.cm>
2019-01-24 22:29:06 -0500

    module1-hw1 lookupByLastName is working

--- CS602_HW1_Kaeneman/employeeModule_v1.js
+++ CS602_HW1_Kaeneman/employeeModule_v1.js
@@ -19,6 +19,22 @@ module.exports = {
             }
         } 
     },
+    /*****************************************************************************
+    * Finds one or more users by searching for matching last names in an array.
+    * Argument: takes a name as a String.
+    * Returns: first name, last name, and id of users in the array, or an empty array.
+    ******************************************************************************/
+    lookupByLastName: (user_lastName) => {
+        user_array = []; // creates an empty array
+        for (var x in data) {
+            if (data[x].lastName == user_lastName) {
+                // populates array if a user is found
+                user_array.push(data[x]);
+            }
+        }
+        return user_array;
+    },    
+
     addEmployee: (firstName, lastName) => {
         data.firstName = firstName;
         data.lastName = lastName;
--- CS602_HW1_Kaeneman/hw1a.js
+++ CS602_HW1_Kaeneman/hw1a.js
@@ -1,3 +1,8 @@
 var foo = require('./employeeModule_v1');
 
-console.log(foo.lookupById(2));
+// console.log(foo.lookupById(2));
+console.log(foo.lookupByLastName('Smith'));
+
+
+// console.log(foo.lookupById(2), 
+  //foo.lookupByLastName('Smith'));
