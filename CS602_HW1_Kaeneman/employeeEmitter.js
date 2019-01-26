77246b123165e40bb650fb149139ceceff0f4eb6
skaeneman <skaeneman@yahoo.cm>
2019-01-25 21:15:31 -0500

    module1-hw1 built class and constructor

--- CS602_HW1_Kaeneman/employeeEmitter.js
+++ CS602_HW1_Kaeneman/employeeEmitter.js
@@ -1,6 +1,15 @@
 const EventEmitter = require('events').EventEmitter;
 const emitter = new EventEmitter();
 
+// creates the class
+class EmployeeEmitter extends EventEmitter {
+    // create a constructor 
+    constructor(args) {
+        super();
+        this.data = args;
+    }
+}
+
 // subscribe 
 emitter.on('event1', function(args){
     console.log('event1 raised args:', args);
