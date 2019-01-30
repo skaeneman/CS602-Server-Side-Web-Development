ac380a6a770206e15fc0b5c23c6c80fcc9feee71
skaeneman <skaeneman@yahoo.com>
2019-01-29 20:56:03 -0500

    module2-part1 working on server.js

--- CS602_HW2_Kaeneman/part1/server.js
+++ CS602_HW2_Kaeneman/part1/server.js
@@ -1,10 +1,11 @@
-
+var employees = require('./employeeModule');
+var _ = require('underscore');
 var colors = require('colors/safe');
 const net = require('net');
 
 // create socket to read from and write to the client
 const server = net.createServer(socket => {
-    console.log(colors.green("Client connected!..."));
+    console.log(colors.red("Client connection..."));
 
     // closes the socket to end the clients connection
     socket.on('end',() => {
@@ -13,7 +14,24 @@ const server = net.createServer(socket => {
 
     // process the data from the client
     socket.on('data', (data) => {
-        console.log(colors.blue("Server received data: " + data));
+        // console.log(typeof data);
+        // convert from an Object to a String
+        var strData = data.toString();
+
+        // split the function name from it's parameter
+        var input = strData.split(" ");
+
+        // console.log("0: " + input[0]);
+        // console.log("1: " + input[1]);
+
+        switch(input[0]) {
+            case "lookupById": 
+                socket.write(colors.blue("...Received " + input));
+                break;
+            default:
+                socket.write("Error, please enter a valid command...");    
+        };
+
     });
 
 }); // ends createServer
