50d69974e3949b5861a843bfe849f5611a13e19a
skaeneman <skaeneman@yahoo.com>
2019-01-31 22:03:20 -0500

    module2-part2 show employee as html and json is working

--- CS602_HW2_Kaeneman/part2/server.js
+++ CS602_HW2_Kaeneman/part2/server.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const app = express();
+const employees = require('./employeeModule_v2');
+
+// to parse request body
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// static resources
+app.use(express.static(__dirname + '/public'));
 
 // handlebars view engine
 const handlebars = require('express-handlebars');
-app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// GET request to the homepage
+// GET homepage
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+// GET employee /id/:id
+app.get('/id/:id', (req, res) => {
+    // convert params to an integer
+    var emp_id = parseInt(req.params.id);
+    
+    res.format({
+        // render as JSON
+        'application/json': () => {
+            res.json(employees.lookupById(emp_id));
+        },
+        // render as HTML
+        'text/html': () => {
+            res.render('employee', {
+                employee: employees.lookupById(emp_id)
+            });
+        }
+
+    });
+
+});
+
 
 app.listen(3000, () => {
     console.log('http://localhost:3000');
