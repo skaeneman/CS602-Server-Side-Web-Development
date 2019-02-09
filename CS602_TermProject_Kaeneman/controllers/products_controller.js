b682bc968e5272e1a63b5d7ef97bcfa4a8b5268a
skaeneman <skaeneman@yahoo.com>
2019-02-08 22:15:02 -0500

    termproject created a script to test populating the products collection

--- CS602_TermProject_Kaeneman/controllers/products_controller.js
+++ CS602_TermProject_Kaeneman/controllers/products_controller.js
@@ -1,3 +1,19 @@
 const DB = require('./models/products_controller.js');
 const Product = DB.getProductModel();
 
+// module.exports.saveProduct =
+//     (req, res, next) => {
+
+//         let employee = new Product({
+//             firstName: req.body.firstName,
+//             lastName: req.body.lastName
+//         });
+
+//         employee.save((err) => {
+//             if (err)
+//                 console.log("Error : %s ", err);
+//             res.redirect('/employees');
+//         });
+
+//     };
+
---
+++ CS602_TermProject_Kaeneman/scripts/populate_products.js
@@ -0,0 +1,63 @@
+/*****************************************************************
+ *  Connects to a MongoDB database, uses the Mongooseschema,
+ *  and creates and saves the data for three products.
+ *****************************************************************/
+
+const mongoose = require('mongoose');
+const credentials = require("./credentials.js");
+
+const dbUrl = 'mongodb://' + credentials.username +
+    ':' + credentials.password + '@' + credentials.host + ':' + credentials.port + '/' + credentials.database;
+
+const connection = mongoose.createConnection(dbUrl, { useNewUrlParser: true });
+
+const ProductDb = require('./models/product.js');
+const Product = ProductDb.getProductModel(connection);
+
+connection.on("open", () => {
+
+    // create and save document objects
+    let product;
+
+    product = new Product({
+        id: 1,
+        name: 'Guitar',
+        description: 'This is a description',
+        price: 1000000,
+        quantity: 666
+    });
+    product.save();
+
+    product = new Product({
+        id: 2,
+        name: 'Drum kit',
+        description: 'This is a description of a drum kit',
+        price: 1000000,
+        quantity: 667
+    });
+    product.save();
+
+    product = new Product({
+        id: 3,
+        name: 'Piano',
+        description: 'This is a description of a piano',
+        price: 1000000,
+        quantity: 668
+    });
+    product.save((err) => {
+        connection.close();
+        if (err) throw err;
+        console.log("Successfully saved products!");
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
