d41f5362ef84035533b284080841754211d57589
skaeneman <skaeneman@yahoo.com>
2019-02-11 21:54:00 -0500

    module4 basic php form test working

--- CS602_HW4_Kaeneman/income_tax_v1.php
+++ CS602_HW4_Kaeneman/income_tax_v1.php
@@ -1,32 +1,52 @@
+ <?php
+    if(isset($_POST['SubmitButton'])){
+        $income = $_POST["income"];
+        $message = $income;
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Income Tax Calculator</title>
-    <link rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <main>
         <h1>Income Tax Calculator</h1>
-        <form action="tax_results.php" method="post">
+		<form method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
             <div id="data">
-                <label>Product Description:</label>
-                <input type="text" name="product_description"><br>
-
-                <label>List Price:</label>
-                <input type="text" name="list_price"><br>
-
-                <label>Discount Percent:</label>
-                <input type="text" name="discount_percent"><span>%</span><br>
+                <label>Your Net Income:</label>
+                <input type="text" name="income"><br>
             </div>
 
             <div id="buttons">
                 <label>&nbsp;</label>
-                <input type="submit" value="Calculate Discount"><br>
+                <input type="submit" name="SubmitButton" value="Submit"><br>
             </div>
+		</form>
 
-        </form>
+		    <?php echo "Your income is " .$message; ?>
     </main>
 </body>
 </html>
+
+
+
+
+
+    <!-- <!DOCTYPE html>
+    <html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form action="#" method="post">
+            <input type="number" name="price"> <br>
+            <input type="number" name="qty"><br>
+            <input type="submit" name="SubmitButton">
+        </form>
+
+    </body>
+    </html> -->
