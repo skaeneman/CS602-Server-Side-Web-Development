1e0a93f77a66e90cbdbe0c92cd186b4bab696ccd
skaeneman <skaeneman@yahoo.com>
2019-02-16 12:58:28 -0500

    module4-part2 complete.  added ability to find the max index position and cleaned up code formatting.

--- CS602_HW4_Kaeneman/income_tax_v2.php
+++ CS602_HW4_Kaeneman/income_tax_v2.php
@@ -1,112 +1,83 @@
  <?php
 
-	define ('TAX_RATES',
-	  array(
-		'Single' => array(
-		  'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
-		  'Ranges' => array(0, 9275, 37650, 91150, 190150, 413350, 415050),
-		  'MinTax' => array(0, 927.50, 5183.75, 18558.75, 46278.75, 119934.75, 120529.75)
-		  ),
-		'Married_Jointly' => array(
-		  'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
-		  'Ranges' => array(0, 18550, 75300, 151900, 231450, 413350, 466950),
-		  'MinTax' => array(0, 1855.00, 10367.50, 29517.50, 51791.50, 111818.50, 130578.50)
-		  ),
-		'Married_Separately' => array(
-		  'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
-		  'Ranges' => array(0, 9275, 37650, 75950, 115725, 206675, 233475),
-		  'MinTax' => array(0, 927.50, 5183.75, 14758.75, 25895.75, 55909.25, 65289.25)
-		  ),
-		'Head_Household' => array(
-		  'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
-		  'Ranges' => array(0, 13250, 50400, 130150, 210800, 413350, 441000),
-		  'MinTax' => array(0, 1325.00, 6897.50, 26835.00, 49417, 116258.50, 125936)
-		  )
-		)
-	);
+		define ('TAX_RATES',
+			array(
+			'Single' => array(
+				'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
+				'Ranges' => array(0, 9275, 37650, 91150, 190150, 413350, 415050),
+				'MinTax' => array(0, 927.50, 5183.75, 18558.75, 46278.75, 119934.75, 120529.75)
+				),
+			'Married_Jointly' => array(
+				'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
+				'Ranges' => array(0, 18550, 75300, 151900, 231450, 413350, 466950),
+				'MinTax' => array(0, 1855.00, 10367.50, 29517.50, 51791.50, 111818.50, 130578.50)
+				),
+			'Married_Separately' => array(
+				'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
+				'Ranges' => array(0, 9275, 37650, 75950, 115725, 206675, 233475),
+				'MinTax' => array(0, 927.50, 5183.75, 14758.75, 25895.75, 55909.25, 65289.25)
+				),
+			'Head_Household' => array(
+				'Rates' => array(10, 15, 25, 28, 33, 35, 39.6),
+				'Ranges' => array(0, 13250, 50400, 130150, 210800, 413350, 441000),
+				'MinTax' => array(0, 1325.00, 6897.50, 26835.00, 49417, 116258.50, 125936)
+				)
+			)
+		);
 
-	/***********************************************************************************
-	 *  This function will calculate taxes owed at various income levels 
-	 *  $taxableIncome is the salary earned by an individual
-	 *  $filingStatus is Single, Married_Jointly, Married_Separately, or Head_Household
-	 ***********************************************************************************/
-	function incomeTax($taxableIncome, $filingStatus) {
-		$ranges = TAX_RATES[$filingStatus]['Ranges'];
-		$minTax = TAX_RATES[$filingStatus]['MinTax'];
-		$rates = TAX_RATES[$filingStatus]['Rates'];
+		/***********************************************************************************
+		 *  This function will calculate taxes owed at various income levels 
+		 *  $taxableIncome is the salary earned by an individual
+		 *  $filingStatus is Single, Married_Jointly, Married_Separately, or Head_Household
+		 ***********************************************************************************/
+		function incomeTax($taxableIncome, $filingStatus) {
+			$ranges = TAX_RATES[$filingStatus]['Ranges'];
+			$minTax = TAX_RATES[$filingStatus]['MinTax'];
+			$rates = TAX_RATES[$filingStatus]['Rates'];
 
-		// get the greatest index in the array
-		$maxIndex = max(array_keys($ranges));
+			// get the greatest index in the array
+			$maxIndex = max(array_keys($ranges));
 
-		$taxesOwed = null;
+			$taxesOwed = null;
 
-		foreach ($ranges as $index => $range) {
+			foreach ($ranges as $index => $range) {
 
-			// echo $ranges[$index] + 1 .'$ranges[$index + 1]';
+				// if taxableIncome is greater than the maximum range value  
+				if($taxableIncome > $ranges[$maxIndex]) {								
+					// convert the rate at the max index to a decimal
+					$rateDecimal = $rates[$maxIndex] / 100;
+					// determine taxes owed
+					$taxesOwed = (($taxableIncome - $ranges[$maxIndex]) * $rateDecimal) + $minTax[$maxIndex]; 
+					break;
+				}
 
-			// if taxableIncome equals one of the ranges or ranges + 1
-			// if(($taxableIncome == $range) or ($taxableIncome == ($ranges[$index] + 1))) {
-								
-			// 	echo $rates[$index - 1]. "% equal to range in array <br>";
-			// 	echo $index - 1 ." index - 1 <br>";
-
-			// 	$rateDecimal = $rates[$index - 1] / 100; // convert to decimal
-			// 	$taxesOwed =  (($taxableIncome - $ranges[$index]) * $rateDecimal) + $minTax[$index - 1];
-
-			// 	break;
-			// } 
-
-			// if taxableIncome is greater than the maximum range value  
-			if($taxableIncome > $ranges[$maxIndex]) {			
-				
-				// echo $rates[$maxIndex] ."% over array values <br>";
-				// echo $maxIndex ." maxIndex <br>";
-
-				// convert the rate at the max index to a decimal
-				$rateDecimal = $rates[$maxIndex] / 100;
-				// determine taxes owed
-				$taxesOwed = (($taxableIncome - $ranges[$maxIndex]) * $rateDecimal) + $minTax[$maxIndex]; 
-				break;
-			}
-
-			// if taxableIncome falls within the ranges in the array
-			elseif(($taxableIncome <= $range) and ($taxableIncome > $ranges[$index - 1])) {
-								
-				// echo $rates[$index - 1]. "% in range of array <br>";
-				// echo $index - 1 ." (index - 1) <br>";
-
-				$rateDecimal = $rates[$index - 1] / 100; // convert to decimal
-				$taxesOwed =  (($taxableIncome - $ranges[$index]) * $rateDecimal) + $minTax[$index];
-
-				break;
-			}   
-		}  
-		return $taxesOwed;
-	};
-
-	// $out = incomeTax(1000000, 'Single');
-	// echo "$".number_format($out, 2) ." taxes owed";
+				// if taxableIncome falls within the ranges in the array
+				elseif(($taxableIncome <= $range) and ($taxableIncome > $ranges[$index - 1])) {									
+					$rateDecimal = $rates[$index - 1] / 100; // convert to decimal
+					$taxesOwed =  (($taxableIncome - $ranges[$index]) * $rateDecimal) + $minTax[$index];
+					break;
+				}   
+			}  
+			return $taxesOwed;
+		};
 
 
+		// get income from user input
+		if(isset($_POST['SubmitButton'])) {
+			$incomeInput = $_POST["income"];
+		}
+		
+		// hide tax table output by default
+		$showOutput = FALSE;
 
-	// get income from user input
-      if(isset($_POST['SubmitButton'])) {
-	  $incomeInput = $_POST["income"];
-	}
-	
-	// hide tax table output by default
-	$showOutput = FALSE;
+		// variable to check user input
+		$isInputNumeric = null;
 
-	// variable to check user input
-	$isInputNumeric = null;
-
-	// if the form was submitted
-    	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		// if the form was submitted
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
-		// echo gettype($incomeInput);
-
-	  	// check user input to see if it's valid number
+		// check user input to see if it's valid number
 		if (filter_var($incomeInput, FILTER_VALIDATE_FLOAT)) {
 			$isInputNumeric = TRUE;
 			// show output if user entered a number
@@ -115,19 +86,8 @@
 			$isInputNumeric = FALSE;
 		}
 
-		// $incomeSingle =  incomeTaxSingle($incomeInput);
-		// $incomeTaxMarriedJointly = incomeTaxMarriedJointly($incomeInput, 2);
-		// $incomeTaxMarriedSeparately = incomeTaxMarriedSeparately($incomeInput, 2);
-		// $incomeTaxHeadOfHousehold = incomeTaxHeadOfHousehold($incomeInput, 2);
-
-		// $output = incomeTax($incomeInput, 'Single');
-		// echo "$".number_format($output, 2) ." taxes owed";		
-
 	}// $_SERVER
 ?>
-
-
-
 
 
 <!DOCTYPE html>
@@ -246,8 +206,12 @@
 									echo '</td>
 											</tr>';
 
-									// loop to print the remaining rows, except the last row
-									$loopCount = count(TAX_RATES[$index]['Ranges']);						
+									// count the number of array elements 
+									$loopCount = count(TAX_RATES[$index]['Ranges']);		
+									// get the highest index position in the array
+									$maxTaxIndex = max(array_keys(TAX_RATES[$index]['Rates']));
+
+									// loop to print the remaining rows, except the last row			
 									// start loop at 1 to skip first row, use -1 with $loopCount to skip last row
 									for($i=1; $i < ($loopCount - 1); $i++) {
 								
@@ -267,14 +231,14 @@
 										// print the last array element row that was left out of the for loop
 										echo '<tr>
 														<td scope="row">';		
-															// print first row 						
-															echo "$" .number_format((TAX_RATES[$index]['Ranges'][6]) + 1);
-															echo " or more";
+															// print last row 						
+															echo "$" .number_format((TAX_RATES[$index]['Ranges'][$maxTaxIndex]) + 1);
+															echo " or more"; 
 											echo '</td>
 														<td>';
-															echo "$" .number_format(TAX_RATES[$index]['MinTax'][6], 2) ." plus ";
-															echo TAX_RATES[$index]['Rates'][6] ."% of the amount over ";
-															echo "$" .number_format(TAX_RATES[$index]['Ranges'][6]);															
+															echo "$" .number_format(TAX_RATES[$index]['MinTax'][$maxTaxIndex], 2) ." plus ";
+															echo TAX_RATES[$index]['Rates'][$maxTaxIndex] ."% of the amount over ";
+															echo "$" .number_format(TAX_RATES[$index]['Ranges'][$maxTaxIndex]);		
 											echo '</td>
 													</tr>															
 								  </tbody>
