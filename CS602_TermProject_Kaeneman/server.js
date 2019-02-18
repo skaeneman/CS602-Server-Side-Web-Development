8e5fdac1b3a8459c5a462f39ad8f7441550f6426
skaeneman <skaeneman@yahoo.com>
2019-02-17 23:52:09 -0500

    termproject created navbar in bootstrap with shopping cart icon

--- CS602_TermProject_Kaeneman/server.js
+++ CS602_TermProject_Kaeneman/server.js
@@ -51,6 +51,12 @@ app.use(validator());
 app.use(session({ secret: 'secret_pass', resave: false, saveUninitialized: false }));
 app.use(flash());
 
+// allow sessions to be used in any template
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
 // Routing
 var routes = require('./routes/index');
 app.use('/', routes);
