c2dd5c64b2bd2c86b8904651808feba3e940d528
skaeneman <skaeneman@yahoo.com>
2019-03-04 16:40:53 -0500

    term project added error handling to REST routes

--- CS602_TermProject_Kaeneman/controllers/rest_controller.js
+++ CS602_TermProject_Kaeneman/controllers/rest_controller.js
@@ -2,6 +2,7 @@ const DB = require('../models/product.js');
 const Product = DB.getProductModel();
 
 
+
 // Get products and return JSON or XML
 module.exports.getProducts =
     (req, res, next) => {
@@ -24,14 +25,12 @@ module.exports.getProducts =
             // call function in product model to get xml
             var prodXml = DB.getXmlProducts(products);
             res.type('application/xml');
-            res.send(prodXml);
+            res.end(prodXml);
         }
-        // // handle error if format is not JSON or XML
-        // else {
-        //     req.flash('errorMessage', "oops...that format type is not available");
-        //     res.render('products/displayProducts');
-        // }
+        // handle error if format is not JSON or XML
+        else {
+            res.end("<br /><div class='container'><div class='row'><div class='col-sm-12'><div><h3>404 - Not Found</h3><br><img src='/images/404_error.jpg' alt='404 error'></div></div></div></div>");
+        }
 
     });
 };  
-
