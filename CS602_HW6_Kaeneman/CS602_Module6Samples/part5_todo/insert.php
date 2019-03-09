aefb80a093d337eef0cd1cf9bc013ee560190559
skaeneman <skaeneman@yahoo.com>
2019-03-08 22:12:29 -0500

    termproject completed json functionality

--- CS602_HW6_Kaeneman/CS602_Module6Samples/part5_todo/insert.php
+++ CS602_HW6_Kaeneman/CS602_Module6Samples/part5_todo/insert.php
@@ -4,12 +4,23 @@
 	if(isset($_POST['submit'])) {
 
 		// Write the code here
-
-
 		
+		$fName = filter_input(INPUT_POST, 'firstName');
+		$lName = filter_input(INPUT_POST, 'lastName');
+		 
+		$json = file_get_contents('bu_students.json');
+        $students = json_decode($json, true);
+        $student = array('firstName' => $fName, 'lastName' => $lName);
+        $students['students'][] = $student;
 		
-		header('Location:success.php');
+
+		$json_data = json_encode($students);
+		echo $json_data;
+        // file_put_contents('bu_students.json', $json_data);
+
+		// header('Location:success.php');
 	} else {
 		echo 'Sorry, nothing was submitted.';
 	}
 ?>
+
--- CS602_HW6_Kaeneman/CS602_Module6Samples/part5_todo/view.php
+++ CS602_HW6_Kaeneman/CS602_Module6Samples/part5_todo/view.php
@@ -1,11 +1,15 @@
 <?php
 	include('../common.inc.php');
 
-	$file = 'bu_students.json';
-	$str_data = file_get_contents($file);
-	$json_data = json_decode($str_data,true);
+	// $file = 'bu_students.json';
+	// $str_data = file_get_contents($file);
+	// // $json_data = json_decode($str_data,true);
+	// $students = json_decode($str_data,true);
 		
-	pr_dump($json_data['students']);;
+	
+	// foreach($students->student as $student) {
+	// 	echo "<p>" . $student->firstName . " " . $student->lastName . "</p>";
+	// }
 
-
+	// pr_dump($json_data['students']);;
 ?>
